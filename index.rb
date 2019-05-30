92398af0e83b055d594ff32ceec5db951112b45c
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-05-30 08:36:38 +0800

    [update]Modify index.rb
    Learned how to use variable.

--- index.rb
+++ index.rb
@@ -1,5 +1,10 @@
-# 変数nameに、文字列「にんじゃわんこ」を代入してください
-name = "にんじゃわんこ"
+text = "プログラミングを学ぼう"
 
-# 変数nameを出力してください
-puts name
+# 文字列「Progateで」と変数textを連結して、出力してください
+puts "Progateで" + text
+
+length = 8
+width = 9
+
+# 変数lengthと変数widthを掛けて出力してください
+puts length * width
