c341cafe8662aba6e4838f04e5611de38e6e1837
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-02 23:15:37 +0800

    [update]Modify index.rb
    Learned arrays index.

--- index.rb
+++ index.rb
@@ -1,7 +1,9 @@
 # encoding: utf-8
 
-# 変数languagesに、複数の文字列を要素に持つ配列を代入してください
 languages = ["日本語", "英語", "スペイン語"]
 
-# 変数languagesを出力してください
-puts languages
+# インデックス番号が1の要素を出力してください
+puts languages[1]
+
+# インデックス番号が0の要素を使って出力してください
+puts "#{languages[0]}を話せます"
