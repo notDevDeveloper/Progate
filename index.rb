d4e2200f88bb635eb40218ef90d4e23befcfc55b
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-02 23:33:07 +0800

    [update]Modify index.rb
    Learned how to use hash.

--- index.rb
+++ index.rb
@@ -1,7 +1,18 @@
 # encoding: utf-8
 
-# 変数examにハッシュを代入してください
 exam = { "subject" => "Math", "score" => 80 }
 
-# 変数examを出力してください
-puts exam
+# キー「"subject"」の値を出力してください
+puts exam["subject"]
+
+# キー「"subject"」の値を「"Science"」に更新してください
+exam["subject"] = "Science"
+
+# もう一度、キー「"subject"」の値を出力してください
+puts exam["subject"]
+
+# キーが「"grade"」、値が「"good"」の要素を追加してください
+exam["grade"] = "good"
+
+# キー「"grade"」の値を出力してください
+puts exam["grade"]
