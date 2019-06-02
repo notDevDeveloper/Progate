644bf86420ff1f683a0e71847871e87d5c17da60
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-02 23:35:31 +0800

    [update]Modify index.rb
    Learned symbol.

--- index.rb
+++ index.rb
@@ -1,18 +1,7 @@
 # encoding: utf-8
 
-exam = { "subject" => "Math", "score" => 80 }
+# キーをシンボルで書き換えてください
+exam = { :subject => "Math", :score => 80 }
 
-# キー「"subject"」の値を出力してください
-puts exam["subject"]
-
-# キー「"subject"」の値を「"Science"」に更新してください
-exam["subject"] = "Science"
-
-# もう一度、キー「"subject"」の値を出力してください
-puts exam["subject"]
-
-# キーが「"grade"」、値が「"good"」の要素を追加してください
-exam["grade"] = "good"
-
-# キー「"grade"」の値を出力してください
-puts exam["grade"]
+# キー「:score」の値を出力してください
+puts exam[:score]
