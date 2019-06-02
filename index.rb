00d5483b01cade881333984310cb5f00dd783a18
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-02 23:24:41 +0800

    [update]Modify index.rb
    Learned scope.

--- index.rb
+++ index.rb
@@ -1,8 +1,12 @@
 # encoding: utf-8
 
 languages = ["日本語", "英語", "スペイン語"]
+border = "---------------------"
 
-# each文を用いて、要素ごとに「○○を話せます」と出力してください
 languages.each do |language|
+  # 変数borderを出力してください
+  puts border
   puts "#{language}を話せます"
 end
+
+# 以下のコードを削除してください
