88352fc9422c1bafc6fde18d244cb585f5be97c2
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-02 23:21:00 +0800

    [update]Modify index.rb
    Learned each.

--- index.rb
+++ index.rb
@@ -2,8 +2,7 @@
 
 languages = ["日本語", "英語", "スペイン語"]
 
-# インデックス番号が1の要素を出力してください
-puts languages[1]
-
-# インデックス番号が0の要素を使って出力してください
-puts "#{languages[0]}を話せます"
+# each文を用いて、要素ごとに「○○を話せます」と出力してください
+languages.each do |language|
+  puts "#{language}を話せます"
+end
