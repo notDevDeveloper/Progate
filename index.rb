d6f3766559f240680e295679a146017862bfce98
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-02 23:37:38 +0800

    [update]Modify index.rb
    Learned how to write hash key.

--- index.rb
+++ index.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 
-# キーをシンボルで書き換えてください
-exam = { :subject => "Math", :score => 80 }
+# 省略した書き方で書き換えてください
+exam = { subject: "Math", score: 80 }
 
-# キー「:score」の値を出力してください
-puts exam[:score]
+puts exam[:subject]
