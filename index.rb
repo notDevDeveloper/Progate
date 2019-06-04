d9528a522c1d7b9a1a4e3ea9b81dbac18605b96c
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-04 17:39:58 +0800

    [update]Modify index.rb
    learned class definition.

--- index.rb
+++ index.rb
@@ -1,10 +1,5 @@
 # encoding: utf-8
 
-# キーワード引数を使うように書き換えてください
-def buy(item:, price:, count:)
-  puts "#{item}を#{count}台のお買い上げです"
-  puts "合計金額は#{price * count}円です"
+# Menuクラスを定義してください
+class Menu
 end
-
-# キーワード引数を使うように書き換えてください
-buy(item: "テレビ", price: 15000, count: 2)
