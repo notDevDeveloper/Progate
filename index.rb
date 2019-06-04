3153f0a8911a59021ff7b66a1ac9f39c80616082
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-04 14:21:45 +0800

    [update]Modify index.rb
    Learned verious return value.

--- index.rb
+++ index.rb
@@ -1,14 +1,13 @@
 # encoding: utf-8
 
-def discount(price)
-  # 「price / 2」を戻り値として返してください
-  return price / 2
+# shipping_free?メソッドを定義してください
+def shipping_free?(price)
+  return price >= 5000
 end
 
-puts "テレビがセール中です！"
-
-# discountメソッドの戻り値を変数half_priceに代入してください
-half_price = discount(15000)
-
-# 「特別価格で〇〇円です」となるように出力してください
-puts "特別価格で#{half_price}円です"
+# if文の条件式でshipping_free?メソッドを呼び出してください
+if shipping_free?(3000)
+  puts "5000円以上のお買い上げなので送料はいただきません"
+else
+  puts "追加で送料をいただきます"
+end
