d7d19667d006b64c3e7ae2f385808a6186313602
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-04 09:28:36 +0800

    [add]Modify index.rb
    Learned method merit.

--- index.rb
+++ index.rb
@@ -1,9 +1,13 @@
 # encoding: utf-8
 
-def introduce(name)
-  puts "こんにちは"
-  puts "私は#{name}です"
+# print_infoメソッドを定義してください
+def print_info(item)
+  puts "わんこでんきへようこそ！"
+  puts "今日は#{item}がセール中です！"
 end
 
-# 引数を渡してメソッドを呼び出してください
-introduce("にんじゃわんこ")
+# 引数を「ヘッドホン」としてメソッドを呼び出してください
+print_info("ヘッドホン")
+
+# 引数を「テレビ」としてメソッドを呼び出してください
+print_info("テレビ")
