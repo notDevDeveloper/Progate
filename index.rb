bc21e2939627a110858a5cc51418ae6ae956dd10
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-04 17:51:33 +0800

    [update]Modify index.rb
    Learned multiple instance.

--- index.rb
+++ index.rb
@@ -10,8 +10,20 @@ menu1 = Menu.new
 menu1.name = "ピザ"
 puts menu1.name
 
-# menu1のpriceに「800」を代入してください
 menu1.price = 800
-
-# menu1のpriceを出力してください
 puts menu1.price
+
+# Menuクラスのインスタンスを生成して変数menu2に代入してください
+menu2 = Menu.new
+
+# menu2のnameに「すし」を代入してください
+menu2.name = "すし"
+
+# menu2のnameを出力してください
+puts menu2.name
+
+# menu2のpriceに「1000」を代入してください
+menu2.price = 1000
+
+# menu2のpriceを出力してください
+puts menu2.price
