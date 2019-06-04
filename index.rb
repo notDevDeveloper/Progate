558c0c03f4042604cfbf71ef38194b3491e8ad47
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-04 17:29:56 +0800

    [update]Modify index.rb
    Learned keyword argument.

--- index.rb
+++ index.rb
@@ -1,16 +1,10 @@
 # encoding: utf-8
 
-def price_with_shipping(price)
-  # priceが5000以上のとき、戻り値としてpriceを返すif文を作成してください
-  if price >= 5000
-    return price
-  end
-  # priceに500を加えた値を戻り値として返してください
-  return price + 500
+# キーワード引数を使うように書き換えてください
+def buy(item:, price:, count:)
+  puts "#{item}を#{count}台のお買い上げです"
+  puts "合計金額は#{price * count}円です"
 end
 
-puts "商品の合計金額は3000円です"
-puts "お支払い金額は、送料込みで#{price_with_shipping(3000)}円です"
-puts "-----------"
-puts "商品の合計金額は10000円です"
-puts "お支払い金額は、送料込みで#{price_with_shipping(10000)}円です"
+# キーワード引数を使うように書き換えてください
+buy(item: "テレビ", price: 15000, count: 2)
