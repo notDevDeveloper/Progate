a8240990368416786977cb792d588d9e49bdd293
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-04 09:31:55 +0800

    [update]Modify index.rb
    Learned method scope.

--- index.rb
+++ index.rb
@@ -1,13 +1,10 @@
 # encoding: utf-8
 
-# print_infoメソッドを定義してください
 def print_info(item)
   puts "わんこでんきへようこそ！"
   puts "今日は#{item}がセール中です！"
 end
 
-# 引数を「ヘッドホン」としてメソッドを呼び出してください
 print_info("ヘッドホン")
 
-# 引数を「テレビ」としてメソッドを呼び出してください
-print_info("テレビ")
+# 以下のコードを削除してください
