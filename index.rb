aa6a323faeaaa21893ebe1db5b24184e1bf10c03
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-05 17:44:46 +0800

    [add]Create menu.rb
    Learned to divide file.

--- index.rb
+++ index.rb
@@ -1,29 +1,9 @@
 # encoding: utf-8
 
-class Menu
-  attr_accessor :name
-  attr_accessor :price
-
-  # initializeメソッドを書き換えてください
-  def initialize(name:, price:)
-    self.name = name
-    self.price = price
-  end
-
-  def info
-    return "#{self.name} #{self.price}円"
-  end
-
-  def get_total_price(count)
-    total_price = self.price * count
-    if count >= 3
-      total_price -= 100
-    end
-    return total_price
-  end
-end
-
-# 引数を渡してインスタンスを生成してください
+# Menuクラスをmenu.rbに移動して、このファイルからは削除してください
+# Menuクラスを移動したら、menu.rbを読み込んでください
+require "./menu"
+# 以下のコードは移動させないでください
 menu1 = Menu.new(name: "すし", price: 1000)
 
 puts menu1.info
