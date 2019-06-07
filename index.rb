d43b446cfccfb0aa524d67f03a6a06071d407247
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-07 09:15:47 +0800

    [add]Create a menu.rb
    Finished progate course 4.

--- index.rb
+++ index.rb
@@ -1,21 +1,36 @@
 # encoding: utf-8
 
-puts "名前を入力してください"
+require "./menu"
 
-# 入力を受け取って変数nameに代入してください
-name = gets.chomp
+menu1 = Menu.new(name: "ピザ", price: 800)
+menu2 = Menu.new(name: "すし", price: 1000)
+menu3 = Menu.new(name: "コーラ", price: 300)
+menu4 = Menu.new(name: "お茶", price: 200)
 
-# 「○○さん、いらっしゃいませ」となるように出力してください
-puts "#{name}さん、いらっしゃいませ"
+menus = [menu1, menu2, menu3, menu4]
 
-puts "1つ100円のお菓子があります"
-puts "いくつ買いますか?"
+index = 0
+menus.each do |menu|
+  puts "#{index}. #{menu.info}"
+  index += 1
+end
+
+puts "--------------"
+puts "メニューの番号を選択してください"
+
+# 入力を数値として受け取って変数orderに代入してください
+order = gets.chomp.to_i
+
+# 選択されたメニューのインスタンスを変数selected_menuに代入してください
+selected_menu = menus[order]
+
+# 「選択されたメニュー: ○○」となるように出力してください
+puts "選択されたメニュー: #{selected_menu.name}"
+
+puts "個数を入力してください(3つ以上で100円割引)"
 
 # 入力を数値として受け取って変数countに代入してください
 count = gets.chomp.to_i
 
-# 変数total_priceに、100とcountを掛けた値を代入してください
-total_price = 100 * count
-
 # 「お会計は○○円です」となるように出力してください
-puts "お会計は#{total_price}円です"
+puts "お会計は#{selected_menu.get_total_price(count)}円です"
