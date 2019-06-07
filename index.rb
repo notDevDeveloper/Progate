4aa944c82f40f44390f3ab222874472391c69841
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-07 12:30:11 +0800

    [remove]Delete 3 files
    Learned how to use Date class.

--- index.rb
+++ index.rb
@@ -1,32 +1,12 @@
 # encoding: utf-8
 
-require "./food"
-require "./drink"
+require "date"
 
-food1 = Food.new(name: "ピザ", price: 800, calorie: 700)
-food2 = Food.new(name: "すし", price: 1000, calorie: 600)
+# 変数todayに、Date.todayの戻り値を代入してください
+today = Date.today
 
-drink1 = Drink.new(name: "コーラ", price: 300)
-drink1.amount = 500
-drink2 = Drink.new(name: "お茶", price: 200)
-drink2.amount = 400
+# 変数todayをputsしてください
+puts today
 
-menus = [food1, food2, drink1, drink2]
-
-index = 0
-menus.each do |menu|
-  puts "#{index}. #{menu.info}"
-  index += 1
-end
-
-puts "--------------"
-puts "メニューの番号を選択してください"
-order = gets.chomp.to_i
-
-selected_menu = menus[order]
-puts "選択されたメニュー: #{selected_menu.name}"
-
-puts "個数を入力してください(3つ以上で100円割引)"
-count = gets.chomp.to_i
-
-puts "お会計は#{selected_menu.get_total_price(count)}円です"
+# 変数todayに対してsunday?メソッドを用いた結果をputsしてください
+puts today.sunday?
