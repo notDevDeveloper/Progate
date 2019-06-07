47259ffe1e318ea5bf57a5cab8970fc210ffa92f
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-06-07 15:02:50 +0800

    [add]Create 3 files
    Learned class method definition.

--- index.rb
+++ index.rb
@@ -1,12 +1,29 @@
 # encoding: utf-8
 
-require "date"
+require "./food"
+require "./drink"
 
-# 変数todayに、Date.todayの戻り値を代入してください
-today = Date.today
+food1 = Food.new(name: "ピザ", price: 800, calorie: 700)
+food2 = Food.new(name: "すし", price: 1000, calorie: 600)
+drink1 = Drink.new(name: "コーラ", price: 300, amount: 500)
+drink2 = Drink.new(name: "お茶", price: 200, amount: 400)
 
-# 変数todayをputsしてください
-puts today
+menus = [food1, food2, drink1, drink2]
 
-# 変数todayに対してsunday?メソッドを用いた結果をputsしてください
-puts today.sunday?
+index = 0
+menus.each do |menu|
+  puts "#{index}. #{menu.info}"
+  index += 1
+end
+
+puts "--------------"
+puts "メニューの番号を選択してください"
+order = gets.chomp.to_i
+
+selected_menu = menus[order]
+puts "選択されたメニュー: #{selected_menu.name}"
+
+puts "個数を入力してください(3つ以上で100円割引)"
+count = gets.chomp.to_i
+
+puts "お会計は#{selected_menu.get_total_price(count)}円です"
