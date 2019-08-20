63ed36a9fb0bf3ca34bbfd65f8227164762c5b22
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-21 05:54:07 +0800

    [update]Finish Lesson 2

--- Lesson2/script.js
+++ Lesson2/script.js
@@ -1,21 +1,21 @@
-const characters = [
-  { name: "にんじゃわんこ", age: 14 },
-  { name: "ひつじ仙人", age: 100 },
-  { name: "ベイビーわんこ", age: 5 },
-  { name: "とりずきん" }
-];
+const cafe = {
+  name: "Progateカフェ",
+  businessHours: {
+    opening: "10:00(AM)",
+    closing: "8:00(PM)"
+  },
+  // menusプロパティに配列を代入してください
+  menus: ["コーヒー", "紅茶", "チョコレートケーキ"]
+};
 
-for (let i = 0; i < characters.length; i++) {
-  console.log("--------------------");
+console.log(`店名: ${cafe.name}`);
+console.log(
+  `営業時間:${cafe.businessHours.opening}から${cafe.businessHours.closing}`
+);
+console.log(`----------------------------`);
+console.log("おすすめメニューはこちら");
 
-  const character = characters[i];
-
-  console.log(`名前は${character.name}です`);
-
-  // if文を追加してください
-  if (character.age === undefined) {
-    console.log("年齢は秘密です");
-  } else {
-    console.log(`${character.age}歳です`);
-  }
+// for文を用いて配列menusの中身を表示させてください
+for (let i = 0; i < cafe.menus.length; i++) {
+  console.log(cafe.menus[i]);
 }
