ab868819881a552098758942094431c28e5372d1
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-21 05:12:42 +0800

    [update]Learn to update array element

--- Lesson2/script.js
+++ Lesson2/script.js
@@ -1,7 +1,7 @@
 const animals = ["dog", "cat", "sheep"];
 
-// 配列の1つ目の要素を出力してください
-console.log(animals[0]);
+// 配列animalsの3つ目の要素を「rabbit」に更新してください
+animals[2] = "rabbit";
 
-// 配列の3つ目の要素を出力してください
+// 配列animalsの3つ目の要素をコンソールに表示して下さい
 console.log(animals[2]);
