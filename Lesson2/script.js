2b1be77264232f0155b784be173d6d4ad639e34a
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-21 05:45:58 +0800

    [update]Learn to fix undefined

--- Lesson2/script.js
+++ Lesson2/script.js
@@ -2,7 +2,6 @@ const characters = [
   { name: "にんじゃわんこ", age: 14 },
   { name: "ひつじ仙人", age: 100 },
   { name: "ベイビーわんこ", age: 5 },
-  // 要素を追加してください
   { name: "とりずきん" }
 ];
 
@@ -12,5 +11,11 @@ for (let i = 0; i < characters.length; i++) {
   const character = characters[i];
 
   console.log(`名前は${character.name}です`);
-  console.log(`${character.age}歳です`);
+
+  // if文を追加してください
+  if (character.age === undefined) {
+    console.log("年齢は秘密です");
+  } else {
+    console.log(`${character.age}歳です`);
+  }
 }
