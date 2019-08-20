b3df0e8a14b7512d2302f0c9acdeb26ac72e7858
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-21 04:41:43 +0800

    [update]Learn multiple conditional expression

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-const age = 17;
+const age = 24;
 
-// ageの値が10以上20未満のとき、「私は20歳未満ですが、10歳以上です」と出力してください
-if (age >= 20) {
-  console.log("私は20歳以上です");
-} else if (age >= 10) {
-  console.log("私は20歳未満ですが、10歳以上です");
-} else {
-  console.log("私は10歳未満です");
+// 指定された条件のif文を作成してください
+if (age >= 20 && age < 30) {
+  console.log("私は20代です");
 }
