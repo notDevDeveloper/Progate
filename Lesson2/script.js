d6c48cd9ea63efc507b6173677f1d6207410a583
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-21 05:08:14 +0800

    [update]Learn array

--- Lesson2/script.js
+++ Lesson2/script.js
@@ -1,9 +1,5 @@
-// for文を完成させてください
-for (let number = 1; number <= 100; number++) {
-  // if文を用いて、numberが3の倍数の時に「3の倍数です」、そうでないときは数字を出力してください
-  if (number % 3 === 0) {
-    console.log("3の倍数です");
-  } else {
-    console.log(number);
-  }
-}
+// 定数animalsに、指定された配列を代入してください
+const animals = ["dog", "cat", "sheep"];
+
+// 定数animalsを出力して下さい
+console.log(animals);
