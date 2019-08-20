1416fbb94862a880774b39297b8aef119d8d5a93
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-21 03:01:32 +0800

    [update]Learn to update variable value

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-// 変数lengthを定義してください
-let length = 5;
+let number = 7;
+console.log(number);
 
-// 変数lengthの値を出力してください
-console.log(length);
+// 変数numberの値に3を加えてください
+number += 3;
 
-// 変数lengthを用いて、円の面積を出力してください
-console.log(length * length * 3);
+console.log(number);
+
+// 変数numberの値を2で割ってください
+number /= 2;
+
+console.log(number);
