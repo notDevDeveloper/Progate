9ca6636294039f47ba86b775aa05023a635d7a99
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-22 03:49:57 +0800

    [update]Learn to return value application

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -1,10 +1,11 @@
-const half = number => {
-  // numberを2で割った値を戻り値として返してください
-  return number / 2;
+const check = number => {
+  // numberが3の倍数かどうかを戻り値として返してください
+  return number % 3 === 0;
 };
 
-// 定数resultを定義してください
-const result = half(130);
-
-// 「130の半分は〇〇です」となるように出力してください
-console.log(`130の半分は${result}です`);
+// if文の条件式で、checkを呼び出してください
+if (check(123)) {
+  console.log("3の倍数です");
+} else {
+  console.log("3の倍数ではありません");
+}
