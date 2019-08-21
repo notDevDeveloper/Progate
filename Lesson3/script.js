d39661ee47c0d238791a8276a29b7f91939ee103
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-22 03:39:22 +0800

    [update]Learn arrow function

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -1,8 +1,7 @@
-// 定数helloに関数を代入してください
-const hello = function() {
-  console.log("こんにちは!");
-  console.log("私の名前はTaketoshiです");
+// 定数greetにアロー関数を代入してください
+const greet = () => {
+  console.log("こんにちは！");
 };
 
-// 定数helloに代入された関数を呼び出してください
-hello();
+// 定数greetを呼び出してください
+greet();
