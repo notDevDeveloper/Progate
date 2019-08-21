96cd869a2ed84cb96a707ff35ebfbef01884fce5
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-22 03:57:00 +0800

    [update]Finish Lesson 3

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -1,13 +1,23 @@
-// 定数nameを定義してください
-const name = "にんじゃわんこ";
+// 定数dollarYenRateに110を代入してください
+const dollarYenRate = 110;
 
-const introduce = name => {
-  // 「わたしは〇〇です」を出力してください
-  console.log(`わたしは${name}です`);
+// アメリカドルを日本円に換算する関数convertToYenを作成してください
+const convertToYen = priceDollar => {
+  return priceDollar * dollarYenRate;
 };
 
-// 関数introduceを呼び出してください
-introduce("ひつじ仙人");
+const information = (name, price) => {
+  console.log(`アメリカドルで${name}は${price}ドルです`);
 
-// 定数nameの値を出力してください
-console.log(name);
+  // 定数priceYenを用意し、関数convertToYenを呼び出したものを代入してください
+  const priceYen = convertToYen(price);
+
+  // 「日本円で〇〇は△△円です」と出力してください
+  console.log(`日本円で${name}は${priceYen}円です`);
+
+  // 消さないでください
+  console.log("--------------");
+};
+
+information("香水", 48);
+information("お菓子", 6);
