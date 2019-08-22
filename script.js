00ad5ecd73ed285d80d0ef2f2ffa793f4c40e46e
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-22 20:38:01 +0800

    [update]Learn to hide modal

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ $(function() {
     $("#login-modal").fadeIn();
   });
 
-  // 「.signup-show」のclickイベントを作成してください
   $(".signup-show").click(function() {
     $("#signup-modal").fadeIn();
+  });
+
+  // 「.close-modal」要素にclickイベントを設定してください
+  $(".close-modal").click(function() {
+    $("#login-modal").fadeOut();
+    $("#signup-modal").fadeOut();
   });
 });
