d73a32726513d389c764b279378354a074bb50d7
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-22 21:22:30 +0800

    [update]Learn to switch slide to display

--- deliverable2/script.js
+++ deliverable2/script.js
@@ -1,8 +1,10 @@
 $(function() {
-  // 「#hide-btn」要素のclickイベントをつくってください
-  $("#hide-btn").click(function() {
+  $("#second-btn").click(function() {
+    // 「.active」要素からactiveクラスを取り除いてください
+    // 2つ目の「.slide」要素にactiveクラスをつけてください
+    $(".active").removeClass("active");
     $(".slide")
       .eq(1)
-      .fadeOut();
+      .addClass("active");
   });
 });
