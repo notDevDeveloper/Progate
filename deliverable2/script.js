a03b7fbf0f05414f4971c4437b670a9642115b52
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-22 21:37:14 +0800

    [update]Learn prev and next

--- deliverable2/script.js
+++ deliverable2/script.js
@@ -1,13 +1,25 @@
 $(function() {
   $(".index-btn").click(function() {
     $(".active").removeClass("active");
-
-    // 変数clickedIndexを定義し、クリックしたボタンのインデックス番号を代入してください
-    var clickedIndex = $(".index-btn").index(this);
-
-    // eqの引数をclickedIndexに書き換えてください
+    var clickedIndex = $(".index-btn").index($(this));
     $(".slide")
       .eq(clickedIndex)
       .addClass("active");
+  });
+
+  $(".change-btn").click(function() {
+    // 変数$displaySlideを定義してください
+    var $displaySlide = $(".active");
+
+    // 変数$displaySlideからactiveクラスを取り除いてください
+    $displaySlide.removeClass("active");
+
+    // ifとelseを用いて、$displaySlideの前もしくは次の要素に
+    // activeクラスをつけてください
+    if ($(this).hasClass("next-btn")) {
+      $displaySlide.next().addClass("active");
+    } else {
+      $displaySlide.prev().addClass("active");
+    }
   });
 });
