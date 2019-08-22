be081c054e0625775592759cfc33470381675434
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-22 23:20:41 +0800

    [update]Learn to select automatically selection box

--- deliverable3/script.js
+++ deliverable3/script.js
@@ -12,9 +12,13 @@ $(function() {
     return false;
   });
 
-  // 「.option-btn」要素のclickイベントをつくってください。
   $(".option-btn").click(function() {
     var optionText = $(this).text();
-    $("#text-form").val(`${optionText}が好きな理由は、`);
+    // 変数clickedOptionに、クリックした要素のdata-optionの値を代入してください。
+    var clickedOption = $(this).attr("data-option");
+
+    $("#text-form").val(optionText + "が好きな理由は、");
+    // 変数clickedOptionを用いて、「#select-form」の値を自動で入力してください。
+    $("#select-form").val(clickedOption);
   });
 });
