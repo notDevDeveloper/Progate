697a1bf1070c6d20d5c653aaa7501916dfa3e86c
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-22 10:52:49 +0800

    [update]Learn find and children

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 $(function() {
-  // 「.list-item」要素に対するclickイベントを作成してください
-  $(".list-item").click(function() {
-    $(this).css("color", "red");
+  $("#find-method").click(function() {
+    // findメソッドで、「#wrapper」内にあるすべての「a」要素を取得し、
+    // cssメソッドで文字の色をredに指定してください
+    $("#wrapper")
+      .find("a")
+      .css("color", "red");
+  });
+
+  $("#children-method").click(function() {
+    // childrenメソッドで、「#wrapper」の一階層下にある「a」要素を取得し、
+    // fadeOutメソッドで隠してください
+    $("#wrapper")
+      .children("a")
+      .fadeOut();
   });
 });
