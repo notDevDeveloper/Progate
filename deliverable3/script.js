01d23ea2d8546cac6b1f5aad383bcb1e7971a6b3
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-22 23:02:21 +0800

    [update]Learn to get selection box value

--- deliverable3/script.js
+++ deliverable3/script.js
@@ -1,7 +1,12 @@
 $(function() {
-  // 「#form」要素のsubmitイベントを作成してください
-  $("#form").submit(function(e) {
+  $("#form").submit(function() {
+    // 変数selectValueを定義してください。
+    var selectValue = $("#select-form").val();
+
     var textValue = $("#text-form").val();
+    // 「#output-select」要素のテキストを変数selectValueの値で書き換えてください。
+    $("#output-select").text(selectValue);
+
     $("#output-text").text(textValue);
     return false;
   });
