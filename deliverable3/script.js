179b116fbb7a0ba1cf0c99b7b76b25e610aee2c4
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-22 23:14:50 +0800

    [update]Learn to input automatically to input field

--- deliverable3/script.js
+++ deliverable3/script.js
@@ -1,17 +1,20 @@
 $(function() {
   $("#form").submit(function() {
-    var selectValue = $("#select-form").val();
-    var textValue = $("#text-form").val();
-
-    // textValueが空のとき、エラー文を表示してください
-    if (textValue == "") {
+    var selectItem = $("#select-form").val();
+    var textItem = $("#text-form").val();
+    if (textItem == "") {
       $("#error-message").text("理由を記入してください");
     } else {
       $("#error-message").text("");
     }
-
-    $("#output-select").text(selectValue);
-    $("#output-text").text(textValue);
+    $("#output-select").text(selectItem);
+    $("#output-text").text(textItem);
     return false;
+  });
+
+  // 「.option-btn」要素のclickイベントをつくってください。
+  $(".option-btn").click(function() {
+    var optionText = $(this).text();
+    $("#text-form").val(`${optionText}が好きな理由は、`);
   });
 });
