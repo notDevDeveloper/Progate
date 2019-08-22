fe4e26e0f7cb5b5c55a754814f4f74bdc9a2afec
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-22 22:06:45 +0800

    [update]Learn length method

--- deliverable2/script.js
+++ deliverable2/script.js
@@ -1,11 +1,11 @@
 $(function() {
-  // toggleChangeBtn関数を定義してください
   function toggleChangeBtn() {
     var slideIndex = $(".slide").index($(".active"));
     $(".change-btn").show();
     if (slideIndex == 0) {
       $(".prev-btn").hide();
-    } else if (slideIndex == 3) {
+      // 「3」の部分を、lengthメソッドを用いて書き換えてください
+    } else if (slideIndex == $(".slide").length - 1) {
       $(".next-btn").hide();
     }
   }
@@ -16,8 +16,6 @@ $(function() {
     $(".slide")
       .eq(clickedIndex)
       .addClass("active");
-
-    // 以下をtoggleChangeBtn関数にまとめ、関数を呼び出すようにしてください
     toggleChangeBtn();
   });
 
@@ -29,8 +27,6 @@ $(function() {
     } else {
       $displaySlide.prev().addClass("active");
     }
-
-    // 以下をtoggleChangeBtn関数にまとめ、関数を呼び出すようにしてください
     toggleChangeBtn();
   });
 });
