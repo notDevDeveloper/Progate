f76ae06e90c42f69b8552ca4647d726ac997793c
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-08-22 11:23:09 +0800

    [update]Learn hover event

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 $(function() {
-  $("#find-method").click(function() {
-    // findメソッドで、「#wrapper」内にあるすべての「a」要素を取得し、
-    // cssメソッドで文字の色をredに指定してください
-    $("#wrapper")
-      .find("a")
-      .css("color", "red");
-  });
-
-  $("#children-method").click(function() {
-    // childrenメソッドで、「#wrapper」の一階層下にある「a」要素を取得し、
-    // fadeOutメソッドで隠してください
-    $("#wrapper")
-      .children("a")
-      .fadeOut();
-  });
+  // 「#language-wrapper」にhoverしたときのhoverイベントを作成してください
+  $("#language-wrapper").hover(
+    function() {
+      // over
+      $(".language-text").fadeIn();
+    },
+    function() {
+      // out
+      $(".language-text").fadeOut();
+    }
+  );
 });
