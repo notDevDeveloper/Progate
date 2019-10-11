ec211c933cd3b3d1934bce953aa6b5cb0338dbd4
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-10-11 22:36:45 +0800

    [update]Combine condition

--- index.rb
+++ index.rb
@@ -1,12 +1,8 @@
 # encoding: utf-8
 
-score = 73
+score = 96
 
-# scoreの値が60より大きい場合に「まずまずです」と出力されるように書き換えてください
-if score > 80
-  puts "よくできました"
-elsif score > 60
-  puts "まずまずです"
-else
-  puts "がんばりましょう"
+# 「95以上かつ99以下」の場合のif文を作ってください
+if score >= 95 && score <= 99
+  puts "高得点です！次は満点を目指しましょう。"
 end
