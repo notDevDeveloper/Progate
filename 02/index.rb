fc74a661a53b0e8fdafe24ff77a05adb2d886668
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-10-12 23:28:14 +0800

    [update]If nil

--- 02/index.rb
+++ 02/index.rb
@@ -1,9 +1,10 @@
 # encoding: utf-8
 
-exam = { subject: "Math", score: 80 }
+exam = { subject: "Math" }
 
-# キー「:grade」の値を出力してください
-puts exam[:grade]
-
-# nilを出力してください
-puts nil
+# 条件式にキー「:score」の値を使うif文をつくってください
+if exam[:score]
+  puts "#{exam[:subject]}の結果は#{exam[:score]}点です"
+else
+  puts "#{exam[:subject]}の結果は分かりません"
+end
