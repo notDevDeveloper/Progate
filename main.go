b7eb1ad4934bd52fcc178b78ecdad3489b6d6640
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-10-23 07:18:21 +0800

    [update]Else if

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 func main() {
-	money := 100
+	money := 200
 	price := 200
 
-	// elseで条件分岐を追加してください
-	if money >= price {
+	// else ifとelseの条件分岐を追加してください
+	if money > price {
 		println("商品を買うことができます")
+	} else if money == price {
+		println("商品を買うことができますが所持金が0円になります")
 	} else {
 		println("お金が足りません")
 	}
