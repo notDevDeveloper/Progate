75e4ee2a378a0d798bf20c7157fe2c43835d1630
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-10-23 07:09:58 +0800

    [update]Else

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 func main() {
-	x := 7 * 10
-	y := 5 * 6
+	money := 100
+	price := 200
 
-	// xの値が70と等しい場合のif文を作成してください
-	if x == 70 {
-		println("xは70です")
-	}
-
-	// yの値が40と等しくない場合のif文を作成してください
-	if y != 40 {
-		println("yは40ではありません")
+	// elseで条件分岐を追加してください
+	if money >= price {
+		println("商品を買うことができます")
+	} else {
+		println("お金が足りません")
 	}
 }
