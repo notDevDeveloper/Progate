e179174ad5ea8c28c6103c2a2d9fcb125231e25e
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-10-26 20:58:28 +0800

    [update]For

--- 02/main.go
+++ 02/main.go
@@ -3,12 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	name1 := "にんじゃわんこ"
-	name2 := "ひつじ仙人"
-	name3 := "ベイビーわんこ"
-
-	// 改行文字を用いて、それぞれの文字列の末尾で改行してください
-	fmt.Printf("ようこそ、%sさん\n", name1)
-	fmt.Printf("ようこそ、%sさん\n", name2)
-	fmt.Printf("ようこそ、%sさん\n", name3)
+	// 1から100までの整数を出力してください
+	for i := 1; i <= 100; i++ {
+		fmt.Println(i)
+	}
 }
