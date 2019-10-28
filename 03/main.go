d403ebd32aa390e1cb9f51724cfc967eed8ce5e2
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-10-29 01:28:18 +0800

    [update]Return value

--- 03/main.go
+++ 03/main.go
@@ -3,22 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	// 引数に1を渡してください
-	ask(1, "dog")
+	// totalScoreにask関数からの戻り値を代入してください
+	totalScore := ask(1, "dog")
 
-	// 引数に2を渡してください
-	ask(2, "cat")
-
-	// 引数に3を渡してください
-	ask(3, "fish")
+	// 指定されたコードを貼り付けてください
+	fmt.Println("スコア", totalScore)
 }
 
-// 引数numberを追加してください
-func ask(number int, question string) {
+// 戻り値の型を定義してください
+func ask(number int, question string) int {
 	var input string
-	// 下の１行を削除し、指定されたコードを貼り付けてください
 	fmt.Printf("[質問%d] 次の単語を入力してください: %s\n", number, question)
-
 	fmt.Scan(&input)
 	fmt.Printf("%sと入力されました\n", input)
+	// 戻り値の値を追加してください
+	return 10
 }
