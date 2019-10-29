ed5ca9e84be6140599395de39a080d3e085402de
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-10-29 08:58:25 +0800

    [update]Argument and return value

--- 03/main.go
+++ 03/main.go
@@ -4,7 +4,6 @@ import "fmt"
 
 func main() {
 	totalScore := ask(1, "dog")
-	// totalScoreにask関数からの戻り値を足してください
 	totalScore += ask(2, "cat")
 	totalScore += ask(3, "fish")
 
@@ -15,7 +14,15 @@ func ask(number int, question string) int {
 	var input string
 	fmt.Printf("[質問%d] 次の単語を入力してください: %s\n", number, question)
 	fmt.Scan(&input)
-	fmt.Printf("%sと入力されました\n", input)
 
-	return 10
+	if question == input {
+		// 問題文の単語と入力値が一致するときの処理を追加してください
+		fmt.Println("正解!")
+		return 10
+
+	} else {
+		// 問題文の単語と入力値が一致しないときの処理を追加してください
+		fmt.Println("不正解!")
+		return 0
+	}
 }
