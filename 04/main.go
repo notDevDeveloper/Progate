20ed8d7977b6205da959c497ad78a3b44d0baab6
notDevDeveloper <dev_free_prog@yahoo.co.jp>
2019-11-04 14:53:06 +0800

    [update]Use pointer

--- 04/main.go
+++ 04/main.go
@@ -6,10 +6,12 @@ func main() {
 	name := "John"
 
 	fmt.Println(name)
-	fmt.Println(&name)
 	// 変数「namePtr」を定義し、nameのポインタを代入してください
 	var namePtr *string = &name
 
-	// namePtrの値を出力してください
-	fmt.Println(namePtr)
+	// ポインタ型変数「namePtr」を使って、変数nameの値を「Kate」に更新してください
+	*namePtr = "Kate"
+
+	// nameの値を出力してください
+	fmt.Println(name)
 }
